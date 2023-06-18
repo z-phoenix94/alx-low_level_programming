d0757e0c9d3b357ebf6c29e4c2bf3a02d3be91aa
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-18 13:11:00 -0700

    single digit numbers only

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+	return (0);
+}
