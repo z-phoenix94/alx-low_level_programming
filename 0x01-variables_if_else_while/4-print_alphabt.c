b62098b19d78082957f1e95b9bd7d602c5376034
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-18 13:15:27 -0700

    without e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints alphabets except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	char p = n;
+
+	while (n <= 122)
+	{
+
+		if ((n != 101)&& (n != 113))
+		{
+			putchar(p);
+			n++;
+			p++;
+		}
+
+		if (n > 122)
+		{
+			p = '\n';
+			putchar(p);
+		}
+	}
+	return (0);
+}
