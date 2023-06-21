640016362da11dfd128fa7d5c6440cbfc1088394
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-21 11:21:19 -0700

    checking a sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * 
+ * Description: print th sign of a number
+ * @n: number to be checked
+ * 
+ * Return: 1 if positive, -1 if negative, 0 otherwise
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}
