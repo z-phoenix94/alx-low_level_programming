708246934e29b6c633bcab13a2ca605181189aad
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-20 20:08:37 -0700

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include main.h
+
+/**
+ * main - Entry point
+ *
+ * Description: prints putschar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
