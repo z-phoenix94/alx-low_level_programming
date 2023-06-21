5e1f6d9498ff3f13bbfdae569f598f2d65ed5c47
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-21 02:36:21 -0700

    printing lowercase 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Description: prints lowercase letters 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int j;
+        char c = 'a';
+
+	while (j < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		j++;
+	}
+	_putchar('\n');
+}
