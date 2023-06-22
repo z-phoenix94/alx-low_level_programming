3018a00681b61cb7c5e3fff3858922553b73f74c
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-22 13:14:29 -0700

    product of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ *
+ * Description: multiply two integers
+ * @a: integer to be multiplied
+ * @b: integer to be multiplied
+ *
+ * Return: product of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
