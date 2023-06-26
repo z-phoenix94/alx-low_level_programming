98a4318b69ebc6a5ebc0dfaa689413e4d68bee1e
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-26 10:22:06 -0700

    reseting to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - entry point
+ *
+ * Description: resets value to 98
+ *
+ * @n: pointer variable
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
