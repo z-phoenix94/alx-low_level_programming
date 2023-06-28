a854c4e443eacfedfb9aaebb9f66c70f988c0ed1
z-phoenix <fikadu.abdisa@hotmail.com>
2023-06-28 04:25:27 -0700

    copy string into buffer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,12 +13,12 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	while (src[i])
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
-		i++;
 	}
+	dest[i++] = '\0';
 	return (dest);
 }
