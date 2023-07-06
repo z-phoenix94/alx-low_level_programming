a0a50fd5018938bdd86f68223a2e10b4a21e6e0e
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-06 04:46:28 -0700

    printing to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Entry point
+ *
+ * Description: print numbers to 98
+ * @n: number to be checked
+ *
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i >= 10 && i < 98)
+			{
+				printf("%d%d, ", i / 10, i % 10);
+			}
+			else if (i == 98)
+			{
+				printf("%d%d", i / 10, i % 10);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >=98; i--)
+		{
+			if (i >= 10 && i < 98)
+			{
+				printf("%d%d, ", i / 10, i % 10);
+			}
+			else if (i == 98)
+			{
+				printf("%d%d", i / 10, i % 10);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	printf("\n");
+}
