afe65a7e1a8e2f42b13f4b602725f22eed979e96
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-10 02:14:45 -0700

    product of two command line argument numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: multiplies two command line arguments
+ * @argc: counts command line arguments
+ * @argv: holds command line argument names
+ *
+ * Return: 0 on Success, 1 on error
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (-1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul = (atoi(argv[1]) * atoi(argv[2]));
+		}
+		printf("%d\n", mul);
+	}
+	return (0);
+}
