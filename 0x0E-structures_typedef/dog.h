7e8f55c1e1200863f82112ad31a1da15a3523fb0
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-17 10:47:08 -0700

    dog struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - dog structure
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: defines dog struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
