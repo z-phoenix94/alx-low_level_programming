5e2fee70f6709dfc437977b6c61bda185fee484b
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-17 09:02:37 -0700

    printing name of a file it is compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the name of a file that
+ * it compiled from
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
