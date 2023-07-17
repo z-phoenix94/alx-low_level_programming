732c00d1af6b51a48a02540615afa0383462fba8
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-17 12:48:51 -0700

    prinitig dog struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - prints dog struct
+ * @d: dog struct variable
+ *
+ * Description: meta data for dog struct
+ */
+void print_dog(struct dog *d)
+{
+
+	if (d->name == NULL)
+	{
+		printf("Name: (nil)\n");
+	}
+	if (d->age == 0)
+	{
+		printf("Age: (nil)\n");
+	}
+	if (d->owner == NULL)
+	{
+		printf("Owner: (nil)\n");
+	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
