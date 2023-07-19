40ed373b4f5374bd71565ad3e5ea4f50cb879588
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-19 10:24:20 -0700

    functions pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Entry point
+ *
+ * Description: prints a name
+ * @name: pointer to name char
+ * @f: call back function
+ *
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
