b2267b2b86ba1f688d760ec1e2c04fdd5f599897
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-19 11:02:55 -0700

    action on every element of an array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - Entry point
+ *
+ * Description: execute a function given as
+ * parameter on each element of array
+ * @array: array variable
+ * @size: size the of the array
+ * @action: pointer to function
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (size && array)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
