10c74a31e9a395461c44ad4904db95063f300e4b
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-21 06:50:55 -0700

    c the hard way

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if ((get_op_func(argv[2]) == NULL) && (argv[2][1] != '\0'))
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
