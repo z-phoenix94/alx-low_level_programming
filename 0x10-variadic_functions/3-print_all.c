54b0aee53eb1a213c800847f8b3e551999c07c54
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-07-24 11:37:36 -0700

    anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,60 +4,55 @@
 #include <stddef.h>
 
 /**
- * char_print - Entry point
+ * print_all - Entry point
  *
- * Description: prints character
- * @any_t: arguement pointer
+ * Description: prints anything
+ * @format: list of types of argument
  *
  * Return: nothing
  */
 
 void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str, *separator = "";
+
+	va_list any_t;
+
+	va_start(any_t, format);
+
+	if (format)
 	{
-		int i = 0;
-		char *str, *separator = "";
-
-
-		va_list any_t;
-
-
-		va_start(any_t, format);
-
-
-		if (format)
+		while (format[i])
 		{
-			while (format[i])
+			switch (format[i])
 			{
-				switch (format[i])
-				{
-					case 'c':
-						printf("%s%c", separator, va_arg(any_t, int));
-						break;
-					case 'i':
-						printf("%s%d", separator, va_arg(any_t, int));
-						break;
-					case 'f':
-						printf("%s%f", separator, va_arg(any_t, double));
-						break;
-					case 's':
-						str = va_arg(any_t, char *);
-						if (!str)
-						{
-							printf("(nil)");
-						}
-						printf("%s%s", separator, str);
-						break;
-					default:
-						i++;
-						continue;
-				}
-				separator = ", ";
-				i++;
+				case 'c':
+					printf("%s%c", separator, va_arg(any_t, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(any_t, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(any_t, double));
+					break;
+				case 's':
+					str = va_arg(any_t, char *);
+					if (!str)
+					{
+						str = "(nil)";
+					}
+					printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
 			}
+			separator = ", ";
+			i++;
 		}
-
-
-		printf("\n");
-		va_end(any_t);
 	}
+	printf("\n");
+	va_end(any_t);
+}
 
