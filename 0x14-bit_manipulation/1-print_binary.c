9e0440944ee60b4c0f231e8656d8f1a2396d652c
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-08-03 23:57:22 -0700

    integer to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: integer to be changed to binary
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int j, counter = 0;
+	unsigned long int current_val;
+
+	for (j = 31; j >= 0; j--)
+	{
+		current_val = n >> j;
+
+		if (current_val & 1)
+		{
+			_putchar('1');
+			counter++;
+		}
+		else if (counter)
+		{
+			_putchar('0');
+		}
+	}
+	if (!counter)
+		_putchar('0');
+}
