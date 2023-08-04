ecd6b8dd5e4f71abd13d01c81d0b37f44ae9405e
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-08-04 06:29:43 -0700

    setting 0 to an index of a binary

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear - sets value of bit 0 to a given index
+ * @n: pointer to unsigned long int number
+ * @index: index of a binary
+ *
+ * Return: 1 if successs, -1 if failed
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+	{
+		return (-1);
+	}
+	*n = (~(1UL << index) & *n);
+	return (1);
+}
