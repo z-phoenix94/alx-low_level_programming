92cedf77ed9118afecb886c5760522e8e5219a21
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-08-03 23:00:44 -0700

    binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
  * binary_to_uint - converts binary to unsigned int
  * @b: pointer to a string of 0 and 1 chars
  *
- * Return: converted number, 0 if b is NULL
+ * Return: converted number, 0 if b is NULL or b is not 0 or 1
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -13,11 +13,17 @@ unsigned int binary_to_uint(const char *b)
 	int c;
 
 	len = strlen(b);
+	if (b == NULL)
+	{
+		return (0);
+	}
 
 	for (c = len - 1; c >= 0; c--)
 	{
 		if (b[c] != '0' && b[c] != '1')
+		{
 			return (0);
+		}
 		if (b[c] == '1')
 		{
 			i += k;
