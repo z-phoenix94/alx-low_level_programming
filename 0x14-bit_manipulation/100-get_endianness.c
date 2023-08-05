2a6e2353fe452daa56e4cbcc9937b40c74a1053f
z-phoenix94 <fikadu.abdisa@hotmail.com>
2023-08-05 04:30:14 -0700

    endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int j = 1;
+	char *e = (char *)&j;
+
+	return (*e);
+}
